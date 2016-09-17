7f2888e1c9ce643eede39c70ff8cb58bf65b94f3
omgimanerd <alvin.lin.dev@gmail.com>
2016-09-17 17:16:58 -0400

    Added instructions scenes

---
+++ Assets/Script/SceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneLoader : MonoBehaviour {
+
+	public void loadGameplayScene() {
+		Application.LoadLevel("Gameplay");
+	}
+
+	public void loadInstructionsScene() {
+		Application.LoadLevel("Instructions");
+	}
+
+	public void loadMenuScene() {
+		Application.LoadLevel("Menu");
+	}
+}
