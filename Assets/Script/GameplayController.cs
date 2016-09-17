00fcea009dd63a93576a53a1fb1bcdaef9d449c6
Nikhil Verma <nikhilnxvverma1@aol.com>
2016-09-17 03:40:55 -0400

    Handled mouse press drag release event for the fling.Added dynamic prefab

--- Assets/Script/GameplayController.cs
+++ Assets/Script/GameplayController.cs
@@ -5,8 +5,12 @@ public class GameplayController : MonoBehaviour {
 
 	public float time=0f;
 	public float health=100f;
+	public GameObject [] trashItems;
 	private float healthDecaySpeed=1f;
 	private float lastCheckpoint;
+	private GameObject currentTrashItem;
+	private Vector2 initialTouchPoint;
+	private const float MIN_SWIPE_DISTANCE=0.5f;
 
 	// Use this for initialization
 	void Start () {
@@ -15,13 +19,69 @@ public class GameplayController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		time+=Time.deltaTime;
-		health-=healthDecaySpeed*Time.deltaTime;// TODO we can probably swap out the delta time for something else
+		UpdateTimeAndHealth();
+		Vector2 swipeDirection=processMouseInput();
+		if(swipeDirection!=Vector2.zero){
+			Debug.Log("Swiped at "+swipeDirection.x+","+swipeDirection.y);
+		}
+		if(currentTrashItem==null){
+			currentTrashItem=Instantiate(trashItems[0],new Vector3(0.02f,4.37f,0),Quaternion.identity) as GameObject;
+		}
 
+	}
+
+	private void UpdateTimeAndHealth(){
+		time+=Time.deltaTime;
+		health-=healthDecaySpeed*Time.deltaTime*10;// TODO we can probably swap out the delta time for something else
+		
 		//every 10 seconds increase the speed
 		if(time-lastCheckpoint>10){
 			lastCheckpoint=time;
 			healthDecaySpeed+=0.1f;
 		}
+	}
+
+	private Vector2 processMouseInput(){
+		if(Input.GetMouseButton(0)){
+			if(Input.GetMouseButtonDown(0)){
+				//press
+				initialTouchPoint=Input.mousePosition;
+			}else{
+				//drag
+				Vector2 currentTouchPoint=Input.mousePosition;
+				float distance=Vector2.Distance(initialTouchPoint,currentTouchPoint);
+				if(distance>=MIN_SWIPE_DISTANCE){
+					return currentTouchPoint-initialTouchPoint;		
+				}
+			}
+
+		}else{
+			//release
+		}
+			
+//			foreach(Touch touch in Input.touches){
+//				switch(touch.phase){
+//					case TouchPhase.Began:
+//						initialTouchPoint=touch.position;
+//						break;
+//					case TouchPhase.Moved:
+//						Vector2 currentTouchPoint=touch.position;
+//						float distance=Vector2.Distance(initialTouchPoint,currentTouchPoint);
+//						if(distance>=MIN_SWIPE_DISTANCE){
+//							return currentTouchPoint-initialTouchPoint;		
+//						}
+//						break;
+//					case TouchPhase.Ended:
+//						break;
+//					case TouchPhase.Canceled:
+//						break;
+//				}
+//			}
+//		}
+		return Vector2.zero;
+	}
+
+	void OnMouseDown(){
+		Debug.Log("Mouse down");
 	}
 }
--- Assets/Script/MatchBarToHealth.cs
+++ Assets/Script/MatchBarToHealth.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class MatchBarToHealth : MonoBehaviour {
 
+	private float initialScaleX;
+	private GameplayController gameController;
+
 	// Use this for initialization
 	void Start () {
-	
+		initialScaleX=this.transform.localScale.x;
+		gameController=GetComponentInParent<GameplayController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(gameController!=null){
+			float health=gameController.health;
+			transform.localScale=new Vector3(initialScaleX*(health/100),transform.localScale.y,1);
+		}
 	}
 }
