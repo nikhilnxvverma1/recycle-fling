09d1f7d62ae0157d1f9411d5ecd450cc04574c43
omgimanerd <alvin.lin.dev@gmail.com>
2016-09-17 13:16:46 -0400

    UpdateTimer

---
+++ Assets/Model.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Model : MonoBehaviour {
+
+  public float health = 100f;
+  public float time = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	  time += Time.deltaTime;
+    health -= Time.deltaTime;
+	}
+}
